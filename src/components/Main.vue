74fc48d5f3b108edf777bd1162055e1c843f146c
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-01-19 15:46:53 +0100

    add axios

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,44 @@
 <template>
   <main>
-      sono il
+     
   </main>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: 'Main',
+    
+    data(){
+        return{
+            query: 'https://api.themoviedb.org/3/search/movie?api_key=517822c87cc8db164e77c94cc3efd137&query=batman',
+            movie: null,
+            movieFiltered: [],
+        };
+    },
+    computed: {
+    //   filterMovie() {
+
+    //   }  
+    },
+    created(){
+        axios
+        .get(this.query)
+        .then((results) => {
+            this.movie = results.data.results;
+            console.log(this.movie);
+            this.movie.forEach(element => {
+                // console.log(element.title);
+                this.movieFiltered.push(element.title)
+                
+            });
+            
+        })
+        .catch((error) => console.log(error));
+
+        console.log(this.movieFiltered);
+    }
 }
 </script>
 
