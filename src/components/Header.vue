508cd13255fafaa6609168c971b31cd86effea64
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-01-19 19:18:41 +0100

    test

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,29 @@
 <template>
   <header>
-      ciao
-      
+        <input type="text" v-model="textSearch">
+        <button 
+            type="submit"
+            @click="$emit('musicSearch', textSearch); getSearchValue();"
+        >
+            Cerca
+        </button>
   </header>
 </template>
 
 <script>
 export default {
     name:'Header',
+    data(){
+        return{
+            textSearch:'',
+            
+        }
+    },
+    methods:{
+        getSearchValue(){
+            return console.log(this.textSearch);
+        }
+    },
 }
 </script>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,20 @@
 <template>
   <main>
-     
+      <button type="sumbit" 
+        @click="getFilteredMovie(); getAllert();"
+        
+        >
+
+      </button>
+    <ul
+        v-for="(element, index) in movieFiltered"
+        :key="'A'+ index"
+    >
+        <li>{{element.title}}</li>    
+        <li>{{element.original_title}}</li>    
+        <li>{{element.original_language}}</li>    
+        <li>{{element.vote_count}}</li>    
+    </ul> 
   </main>
 </template>
 
@@ -12,9 +26,12 @@ export default {
     
     data(){
         return{
-            query: 'https://api.themoviedb.org/3/search/movie?api_key=517822c87cc8db164e77c94cc3efd137&query=batman',
+            queryStatic: 'https://api.themoviedb.org/3/search/movie?api_key=517822c87cc8db164e77c94cc3efd137&query=batman',
+            textSearch:'',
+            query: '',
             movie: null,
             movieFiltered: [],
+            
         };
     },
     computed: {
@@ -23,22 +40,46 @@ export default {
     //   }  
     },
     created(){
-        axios
-        .get(this.query)
-        .then((results) => {
+        // axios
+        // .get(this.query)
+        // .then((results) => {
+        //     this.movie = results.data.results;
+        //     console.log(this.movie);
+        //     this.movie.forEach(element => {
+        //         // console.log(element.title);
+        //         this.movieFiltered.push(element)
+                
+        //     });
+            
+        // })
+        // .catch((error) => console.log(error));
+
+        // console.log(this.movieFiltered);
+    },
+    methods:{
+        getFilteredMovie(){
+            // this.query = this.queryStatic + this.textSearch
+            // console.log(query);
+            axios
+            .get(this.queryStatic)
+            .then((results) => {
             this.movie = results.data.results;
-            console.log(this.movie);
             this.movie.forEach(element => {
                 // console.log(element.title);
-                this.movieFiltered.push(element.title)
-                
+                this.movieFiltered.push(element)
+                  
             });
             
-        })
-        .catch((error) => console.log(error));
+            })
+            .catch((error) => console.log(error));
 
-        console.log(this.movieFiltered);
-    }
+            console.log(this.movieFiltered);
+            return this.movieFiltered
+        },
+        getAllert(){
+            return console.log('ciao');
+        }
+    },
 }
 </script>
 
